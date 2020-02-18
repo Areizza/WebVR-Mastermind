097cc22674700210f13d8bb384d27e118c1e0365
Priscilla Lo <priscillalo@cmail.carleton.ca>
2020-02-18 01:22:17 -0500

    Added score planes to show ID of object in each score box

--- public/throw-away-component.js
+++ public/throw-away-component.js
@@ -15,21 +15,42 @@ AFRAME.registerComponent('throw-away-component', {
             console.log("upp!!!!!!!!");
 
             let posZ = this.getAttribute("position").z;
+            let posY = this.getAttribute("position").y;
+            let posX = this.getAttribute("position").x;
 
-            if (posZ <= -5) {
-                console.log("score")
+            if (posZ <= -12 && posZ >= -13) {
+
+                if(posY < 3) {
+
+                    var id = this.getAttribute("id");
+
+                    if (posX <= -9 && posX >= -12) {
+                        console.log("score in 1");
+                        console.log(id);
+                        Context_AF.createPlane(1, id);
+                    }
+                    if (posX <= 1.5 && posX >= -1.5) {
+                        console.log("score in 2");
+                        Context_AF.createPlane(2, id);
+                    }
+                    if (posX <= 12 && posX >= 9) {
+                        console.log("score in 3");
+                        Context_AF.createPlane(3, id);
+                    }
+                    
+                    console.log(posY + ", " + posZ);
+                }
             }
-            //Context_AF.createCow();
         });
 
         //make the object larger on hover
-        Context_AF.el.addEventListener('mouseenter', function(event) {
-            Context_AF.el.object3D.scale.set(1.1, 1.1, 1.1);
-        });
+        // Context_AF.el.addEventListener('mouseenter', function(event) {
+        //     Context_AF.el.object3D.scale.set(1.1, 1.1, 1.1);
+        // });
 
-        Context_AF.el.addEventListener('mouseleave', function(event) {
-            Context_AF.el.object3D.scale.set(1.0, 1.0, 1.0);
-        })
+        // Context_AF.el.addEventListener('mouseleave', function(event) {
+        //     Context_AF.el.object3D.scale.set(1.0, 1.0, 1.0);
+        // })
 
         Context_AF.el.addEventListener('collisions', function(event) {
             console.log(event.detail)
@@ -37,25 +58,33 @@ AFRAME.registerComponent('throw-away-component', {
     },
 
     //custom function for creating random cows
-    createCow: function() {
+    createPlane: function(scorePos, id) {
         const Context_AF = this; //be careful of "this"!
 
         //create an html element that loads in cows model
-        let cowElem = document.createElement('a-entity'); //create element by code
-        cowElem.setAttribute('class', 'clickable');
-        cowElem.setAttribute('obj-model', {obj: 'assets/models/Cow.obj'}); //set model
-        cowElem.setAttribute('material', {src: 'assets/textures/Cow.png'}); //set material/texture
+        let displayPlane = document.createElement('a-entity'); //create element by code
+        displayPlane.setAttribute('material', 'color: black; shader: flat;'); //set material/texture
+        displayPlane.setAttribute('text', 'value:' + id + '; width: 20; align: center;');
 
-        //random transforms
-        cowElem.setAttribute('position', {x:(Math.random() * 6.0) - 3.0, y: 0, z:-4.0 - (Math.random() * 3.0)}); //random x around axis and random z behind button, above ground on y=0
+        var planePos;
 
-        const randScale = 0.2 + (Math.random() * 0.8);
-        cowElem.setAttribute('scale', {x:randScale, y:randScale, z:randScale}); //random scale
+        if (scorePos == 1) {
+            planePos = {x: -10.5, y: 4, z: -19};
+        }
+        if (scorePos == 2) {
+            planePos = {x: 0, y: 4, z: -19};
+        }
+        if (scorePos == 3) {
+            planePos = {x: 10.5, y: 4, z: -19};
+        }
 
-        cowElem.setAttribute('rotation', {x:0, y:Math.random() * 360.0, z:0}); //random y rotation
+        displayPlane.setAttribute('position', planePos); //random x around axis and random z behind button, above ground on y=0
+        displayPlane.setAttribute('width', 3);
+        displayPlane.setAttribute('height', 3);
+        displayPlane.setAttribute('scale', {x:1, y:1, z:1}); //random scale
 
         //attach to scene
         let scene = document.querySelector('a-scene');
-        scene.appendChild(cowElem);
+        scene.appendChild(displayPlane);
     }
 });
